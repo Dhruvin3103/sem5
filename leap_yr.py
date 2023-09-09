4208d80ad5b672413de158bc9af41a33798158e0
Dhruvin Chawda <114014218+Dhruvin3103@users.noreply.github.com>
2023-09-09 10:01:46 +0530

    Add files via upload
    hi hello namaste kaise ho kya dekh rahe ho X )

---
+++ leap_yr.py
@@ -0,0 +1,6 @@
+# yr = int(input("Enter the year : "))
+
+# if (yr % 4 ==0 and yr % 100!=0) or (yr % 400==0):
+#     print(True)
+# else:
+#     print(False)
---
+++ pattern.py
@@ -0,0 +1,6 @@
+# n = int(input("Enter the level : "))
+# n = 100
+# for i in range(1,n):
+#     for j in range(1,i+1):
+#         print(i, end=' ')
+#     print()
