352d6e05b8d7716a08523c550f43c9137c7820fd
Dhruvin3103 <dcchawda2003@gmail.com>
2023-10-22 11:17:41 +0530

    some code cleaning

--- AI/Astar.py
+++ AI/Astar.py
@@ -19,9 +19,6 @@ graph_heu = {
 }
 curr_node = 'A' # this is also start node at the beginning
 goal_node = 'Z'
-current_min = 9999999
-
-# def minOpenList()//
 
 def searchlist(l,k):
     for i in l:
@@ -37,12 +34,10 @@ def funcofn(gl,cn): # gl <- path of previous node , cn <- current node
 
 open_list = []
 close_list = []
-# sorted_array = sorted(my_array, key=lambda x: x[0])
 open_list.append([curr_node,graph_heu[curr_node],[curr_node]])
 n = 0 
 try:
     while True:
-        # print(open_list)
         parent_node = open_list[0][0]
         close_list.append(parent_node)
         current_path = open_list[0][2]
@@ -52,7 +47,6 @@ try:
         for j in graph[open_list[0][0]]:
             open_list.append([j,funcofn(current_path,j),current_path+[j]])
             open_list = sorted(open_list, key=lambda x: x[1])
-        # print(open_list,parent_node)
         open_list = [pair for pair in open_list if pair[0] != parent_node]
         n+=1   
         print(f"Open List is : {open_list}\nclosed List is : {close_list}\n")
