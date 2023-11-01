da806ddca12e48642c52d8b85cdcb1837b102138
Dhruvin3103 <dcchawda2003@gmail.com>
2023-11-01 10:40:49 +0530

    chaitya ye lo codee

---
+++ adbms/replica.py
@@ -0,0 +1,115 @@
+import mysql.connector
+import csv
+
+host = "127.0.0.1"
+user = "root"
+password = "3103"
+database = "sakila"
+csv_filename = "output.csv"
+
+try:
+    mydb = mysql.connector.connect(
+        host = host,
+        user = user,
+        password = password,
+        database = database
+    )
+
+    mycursor = mydb.cursor()
+
+    query = "SELECT * FROM actor"
+    mycursor.execute(query)
+
+    myresult = mycursor.fetchall()
+
+    with open(csv_filename, 'w') as csv_file:
+        my_csv_writer = csv.writer(csv_file)
+        my_csv_writer.writerow([i[0] for i in mycursor.description])
+        my_csv_writer.writerows(myresult)
+
+
+    # for x in myresult:
+    #     print(x)
+
+except Exception as e:
+    print(f"Error: {str(e)}")
+
+finally:
+    mycursor.close()
+    mydb.close()import mysql.connector
+import csv
+
+host = "127.0.0.1"
+user = "root"
+password = "password"
+database = "altcrime"
+csv_filename = "output.csv"
+
+try:
+    mydb = mysql.connector.connect(
+        host = host,
+        user = user,
+        password = password,
+        database = database
+    )
+
+    mycursor = mydb.cursor()
+
+    query = "SELECT * FROM crime1"
+    mycursor.execute(query)
+
+    myresult = mycursor.fetchall()
+
+    with open(csv_filename, 'w') as csv_file:
+        my_csv_writer = csv.writer(csv_file)
+        my_csv_writer.writerow([i[0] for i in mycursor.description])
+        my_csv_writer.writerows(myresult)
+
+
+    # for x in myresult:
+    #     print(x)
+
+except Exception as e:
+    print(f"Error: {str(e)}")
+
+finally:
+    mycursor.close()
+    mydb.close()import mysql.connector
+import csv
+
+host = "127.0.0.1"
+user = "root"
+password = "password"
+database = "altcrime"
+csv_filename = "output.csv"
+
+try:
+    mydb = mysql.connector.connect(
+        host = host,
+        user = user,
+        password = password,
+        database = database
+    )
+
+    mycursor = mydb.cursor()
+
+    query = "SELECT * FROM crime1"
+    mycursor.execute(query)
+
+    myresult = mycursor.fetchall()
+
+    with open(csv_filename, 'w') as csv_file:
+        my_csv_writer = csv.writer(csv_file)
+        my_csv_writer.writerow([i[0] for i in mycursor.description])
+        my_csv_writer.writerows(myresult)
+
+
+    # for x in myresult:
+    #     print(x)
+
+except Exception as e:
+    print(f"Error: {str(e)}")
+
+finally:
+    mycursor.close()
+    mydb.close()
