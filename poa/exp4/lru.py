d248776e9c155f620f687f19e9a0452df9cc72da
Dhruvin3103 <dcchawda2003@gmail.com>
2023-12-10 11:40:03 +0530

    just cleaning code

--- poa/exp4/lru.py
+++ poa/exp4/lru.py
@@ -2,32 +2,10 @@ def replace(d, f, nf, pos):
     replaced_pg = ''
     max= -1 
     for i in f:
-    
         l = d[:pos][::-1]
         if l.index(i)>max:
             replaced_pg = i
             max = l.index(i)
-        # diff.append([i,l.index(i)])
-    
-    # replaced_pg = max(diff, key=lambda i:diff[1])
-    # max_delays = [
-    #     max([pos - j if d[j] == f[i] else -1 for j in range(pos, -1, -1)])
-    #     for i in range(nf)
-    # ]
-    
-    # max_delays = []
-    # for i in range(nf):
-    #     delays = []
-    #     for j in range(pos,-1,-1):
-    #         if d[j] == f[i]:
-    #             delays.append(pos-j)
-    #         else:
-    #             delays.append(-1)
-    #     print(delays)
-    #     max_delays.append(max(delays))
-            
-    
-    # return max(range(nf), key=lambda i: max_delays[i])
     return f.index(replaced_pg)
 
 
